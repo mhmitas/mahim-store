48c83fa4ed85b8b36aab46bff7b29f8e79447dca
mhmitas <mahfuzulmitas@gmail.com>
2024-09-29 08:38:13 +0600

    change default image

--- src/components/shared/ProductsSection.jsx
+++ src/components/shared/ProductsSection.jsx
@@ -4,8 +4,8 @@ import ProductCard from './ProductCard'
 const ProductsSection = ({ data = [] }) => {
     return (
         <section className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {data.map(product => (
-                <ProductCard product={product} />
+            {data.map((product, i) => (
+                <ProductCard product={product} key={i} />
             ))}
         </section>
     )
